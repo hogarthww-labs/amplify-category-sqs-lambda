95fafd0a77bff50852d964c5b09dcdca75e6d9ad
Kristian Mandrup <kristian.mandrup@hogarthww.com>
2021-02-15 14:52:53 +0000

    use EJS templating

--- extensions/addTemplate.js
+++ extensions/addTemplate.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { copyFilesToS3 } = require('./helpers/template-staging');
-const { getProjectName, generateQuestions, askLocationQuestions } = require('./helpers/template-question');
+const { getlambdaName, generateQuestions, askLocationQuestions } = require('./helpers/template-question');
 const { yamlParse, yamlDump } = require('yaml-cfn');
 
 module.exports = (context) => {
@@ -19,7 +19,7 @@ async function createTemplate(context){
     let lambdaName = await getLambdaName(context);
     let sqsName = await getSQSName(context);    
     let props = await askLocationQuestions();
-    props.projectName = lambdaName;
+    props.lambdaName = lambdaName;
     props.sqsName = sqsName;
     props.options = options;
     props.root = path.join(__dirname, 'templates/sqs-lambda-template.json')
@@ -40,25 +40,22 @@ async function prepareCloudFormation(context, props){
     await stageRoot(context, props);
 }
 
-function replaceTemplateName(rootTemplate, props) {
-    rootTemplate = rootTemplate.replace('<sqs-name>', props.sqsName)
-    rootTemplate = rootTemplate.replace('<lambda-name>', props.projectName.toUpperCase())
-    return rootTemplate    
+function renderTemplate(rootTemplate, props) {
+    return ejs.render(rootTemplate, props);
 }
-
 
 async function handleYAML(context, props){
     let rootTemplate = yamlParse(fs.readFileSync(props.root,'utf8'));
-    rootTemplate = await prepareTemplate(context, props, rootTemplate);
-    rootTemplate = replaceTemplateName(rootTemplate, props)
+    rootTemplate = renderTemplate(rootTemplate, props)
+    rootTemplate = await prepareTemplate(context, props, rootTemplate);    
     rootTemplate = await generateQuestions(context, rootTemplate);
     fs.writeFileSync(props.root, yamlDump(rootTemplate, null, 4));
 }
 
 async function handleJSON(context, props){
     let rootTemplate = JSON.parse(fs.readFileSync(props.root));
-    rootTemplate = await prepareTemplate(context, props, rootTemplate);
-    rootTemplate = replaceTemplateName(rootTemplate, props)
+    rootTemplate = renderTemplate(rootTemplate, props)
+    rootTemplate = await prepareTemplate(context, props, rootTemplate);    
     rootTemplate = await generateQuestions(context, rootTemplate);
     fs.writeFileSync(props.root, JSON.stringify(rootTemplate, null, 4));
 }
@@ -82,10 +79,10 @@ async function prepareTemplate(context, props, rootTemplate){
             let urlSplit = rootTemplate.Resources[resource].Properties.TemplateURL.split('/');
             /* For making sure that we don't lose filenames */
             if (urlSplit.length == 0){
-                rootTemplate.Resources[resource].Properties.TemplateURL = "https://s3.amazonaws.com/" + targetBucket + "/amplify-cfn-templates/" + props.projectName + "/" + rootTemplate.Resources[resource].Properties.TemplateURL;
+                rootTemplate.Resources[resource].Properties.TemplateURL = "https://s3.amazonaws.com/" + targetBucket + "/amplify-cfn-templates/" + props.lambdaName + "/" + rootTemplate.Resources[resource].Properties.TemplateURL;
             } else {
                 let filename = urlSplit[urlSplit.length-1];
-                rootTemplate.Resources[resource].Properties.TemplateURL = "https://s3.amazonaws.com/" + targetBucket + "/amplify-cfn-templates/" + props.projectName + "/" + filename;
+                rootTemplate.Resources[resource].Properties.TemplateURL = "https://s3.amazonaws.com/" + targetBucket + "/amplify-cfn-templates/" + props.lambdaName + "/" + filename;
             }
         });
     }
@@ -99,24 +96,24 @@ async function stageRoot(context, props){
         {
           dir: '/',
           template: `${props.root}`,
-          target: `${targetDir}/function/${props.projectName}/${props.projectName}-sqs-template.${props.ending}`,
+          target: `${targetDir}/function/${props.lambdaName}/${props.lambdaName}-sqs-template.${props.ending}`,
         },
       ];
       context.amplify.updateamplifyMetaAfterResourceAdd(
         "sqs",
-        props.projectName,
+        props.lambdaName,
         props.options,
       );
       await context.amplify.copyBatch(context, copyJobs, props);
       if (props.isNestedTemplate === "YES"){
         const fileuploads = fs.readdirSync(`${props.nestedFolder}`);
 
-        if (!fs.existsSync(`${targetDir}/function/${props.projectName}/src/`)) {
-            fs.mkdirSync(`${targetDir}/function/${props.projectName}/src/`);
+        if (!fs.existsSync(`${targetDir}/function/${props.lambdaName}/src/`)) {
+            fs.mkdirSync(`${targetDir}/function/${props.lambdaName}/src/`);
         }
 
         fileuploads.forEach((filePath) => {
-            fs.copyFileSync(`${props.nestedFolder}/${filePath}`, `${targetDir}/function/${props.projectName}/src/${filePath}`);
+            fs.copyFileSync(`${props.nestedFolder}/${filePath}`, `${targetDir}/function/${props.lambdaName}/src/${filePath}`);
         });
         copyFilesToS3(context, props.options, props)
       }
